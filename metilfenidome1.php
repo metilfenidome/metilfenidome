ebdf2f2d36b8c5d57008c384c50d474cc4c3219d
metilfenidome <domecort.publicidad@gmail.com>
2016-09-08 11:03:32 -0500

    Add files via upload

---
+++ metilfenidome1.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<title>Metilfenidome</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+.w3-sidenav a,.w3-sidenav h4 {font-weight:bold}
+</style>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+<!-- Sidenav/menu -->
+<nav class="w3-sidenav w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidenav"><br>
+  <div class="w3-container">
+    <a href="#" onClick="w3_close()" class="w3-hide-large w3-right" title="close menu">
+    <img alt="delete" src="images/Delete copy.png">
+    </a>
+    <img alt="pill" src="images/pill.png" style=" align-content:center;width:45%;" class="w3-round"><br><br>
+    <h4 class="w3-padding-0"><b>METIL-FENI-DOME</b></h4>
+    <p class="w3-text-pale-red"></p>
+  </div>
+  <a href="#" class="w3-pale-red w3-padding w3-text-pink">INICIO</a>
+  <a href="#" class="w3-padding">NOSOTROS</a>
+  <a href="#" class="w3-padding">CARRERA</a>
+  <a href="#" class="w3-padding">COLABORA</a>
+  <a href="#" class="w3-padding">CONTACTO</a>
+   
+  <div class="w3-section w3-padding-top w3-large">
+    <a href="#" class="w3-hover-white w3-hover-text-indigo w3-show-inline-block"><i class="fa fa-facebook-official"></i></a>
+    <a href="#" class="w3-hover-white w3-hover-text-red w3-show-inline-block"><i class="fa fa-pinterest-p"></i></a>
+    <a href="#" class="w3-hover-white w3-hover-text-light-blue w3-show-inline-block"><i class="fa fa-twitter"></i></a>
+    <a href="#" class="w3-hover-white w3-hover-text-pink w3-show-inline-block"><i class="fa fa-instagram"></i></a>
+    <a href="#" class="w3-hover-white w3-hover-text-purple w3-show-inline-block"><i class="fa fa-github"></i></a>
+  </div>
+</nav>
+
+<!-- Overlay effect when opening sidenav on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Header -->
+  <header class="w3-container">
+    <a href="#"><i class="w3-centered"><img src="images/logo2.png" alt="logo" style="width:100px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></i></a>
+    <span class="w3-opennav w3-hide-large w3-xxlarge" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <h1 class="w3-right"><strong>METILFENIDOME</strong></h1>
+    <div class="w3-section w3-bottombar w3-padding-16 w3-center">
+      <span class="w3-margin-right"></span>
+      <button class="w3-btn w3-pale-red w3-text-white">ALL</button>
+      <button class="w3-btn w3-white"><em class="w3-margin-right"><img src="images/user_female copy.png"></em>Quien es Metilfenidome?</button>
+      <button class="w3-btn w3-white w3-hide-small"><i class="w3-margin-right"><img src="images/source_code.png"></i>Platicame tu proyecto</button>
+      <button class="w3-btn w3-white w3-hide-small"><i class="w3-margin-right"><img src="images/google_scholar_copyrighted.png"><class="w3-margin-right"></i>Mi blog</button>
+    </div>
+  </header>
+  
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="img_lights.jpg" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="img_nature.jpg" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="img_fjords.jpg" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="img_fjords.jpg" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="img_fjords.jpg" alt="METILFENIDOME" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>METILFENIDOME</b></p>
+        <p>METILFENIDOME</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Pagination -->
+  <div class="w3-center w3-padding-32">
+    <ul class="w3-pagination">
+      <li><a class="w3-pale-red" href="#">1</a></li>
+      <li><a class="w3-hover-pink" href="#">2</a></li>
+      <li><a class="w3-hover-pink" href="#">3</a></li>
+      <li><a class="w3-hover-pink" href="#">4</a></li>
+      <li><a class="w3-hover-pale-red" href="#">>></a></li>
+    </ul>
+  </div>
+
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-white">
+  <div class="w3-row-padding">
+    <div class="w3-third">
+      <h3>Mas Info</h3>
+      <p>bla bla bla</p>
+      <p>Derechos Reservados by <a href="http://www.eskpublicidad.com" target="_blank">Metilfenidome</a></p>
+    </div>
+  
+    <div class="w3-third">
+      <h3>BLOG POSTS</h3>
+      <ul class="w3-ul w3-hoverable">
+        <li class="w3-padding-16">
+          <img src="images/blog.png" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Domenica</span><br>
+          <span>mi ultimo post</span>
+        </li>
+        <li class="w3-padding-16">
+          <img src="images/shopping_bag.png" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">No se todavia</span><br>
+          <span>sabe sabe</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="w3-third">
+      <h3>DE LO QUE SE HABLA AQUÍ</h3>
+      <p>
+        <span class="w3-tag w3-pink w3-margin-bottom">Open source</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">HTML5</span>
+        <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">CSS</span><span class="w3-tag w3-dark-grey w3-small w3-margin-bottom"></span><span class="w3-tag w3-pale-red w3-small w3-margin-bottom">DIY</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Newbies</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Experts</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Shopping</span>
+        <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Eventos</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Games</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Git Repositories</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Blogs de ayuda</span> <span class="w3-tag w3-pale-red w3-small w3-margin-bottom">Trends</span>
+      </p>
+    </div>
+
+  </div>
+  </footer>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidenav
+function w3_open() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+</body>
+</html>
+
